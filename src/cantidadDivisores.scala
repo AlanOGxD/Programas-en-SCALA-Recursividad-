72330c2bf12fceb053f83bb6f4f5024cc759ffb1
AlanOGxD <alanosvaldo88@gmail.com>
2021-02-12 20:06:08 -0600

    Se termino ejemplo cantidad divisores

---
+++ src/cantidadDivisores.scala
@@ -0,0 +1,23 @@
+import scala.io.StdIn.readInt
+
+object cantidadDivisores {
+   def divisores(n:Int, cont:Int, cont2:Int): Unit={
+    var contx=cont2
+    if(cont>0){
+    if(n%cont == 0){
+      println(cont) 
+      contx+=1
+      println("des:"+contx)
+    }
+    divisores(n, cont-1, contx)
+    }else{
+      println("El resultado es: "+cont2)
+    }
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println("Ingrese el numero para conocer sus divisores: ")
+    val n = readInt()
+    divisores(n, n, 0)
+  }
+}
